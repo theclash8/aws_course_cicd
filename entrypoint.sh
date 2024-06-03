4c5b506f0b97b9229b0e9f616469dc86eb178117
Luca Rossi <luca@acido.be>
2024-06-03 16:23:26 +0200

    ho aggiunto un entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,5 +6,6 @@ set -o pipefail
 
 cd /tmp
 git clone https://github.com/theclash8/aws_course_cicd.git
+ls -lah
 cp -Rp aws_course_cicd/asset aws_course_cicd.github/index.html /var/www/html
 rm -rf aws_course_cicd/
